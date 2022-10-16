a71253ba82e038363f8b753fd057b6b3bbfe4850
Sreenija <115453811+Sreenijautla@users.noreply.github.com>
2022-10-16 16:12:20 +0530

    Add files via upload

---
+++ reverse_1.py
@@ -0,0 +1,8 @@
+# Write a Python program that accepts a word from the user and reverse it.
+# Sample Test Case
+# Input : Edyoda
+# output: adoydE
+
+
+word=input("Input : ")
+print("output:", word[::-1])
